afb6c53ed3acf48b644eb552460b174bae2829d8
Tetiana Yasko <taniayasko2015@gmail.com>
2021-07-26 19:43:35 +0300

    Updates

--- precache-manifest.9169ea1f7dc63c619263ea7ba322d446.js
+++ precache-manifest.9169ea1f7dc63c619263ea7ba322d446.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab2a146f12e2cca328ac5781407498b4",
+    "revision": "f980aaa17205c83b343fd14961750c0e",
     "url": "./index.html"
   },
   {
-    "revision": "15402f00778019906b92",
+    "revision": "b63cac65fcd072a22b4c",
     "url": "./static/css/main.2ab2c441.chunk.css"
   },
   {
-    "revision": "d16903c2ec5d390d7ac4",
-    "url": "./static/js/2.007983b3.chunk.js"
+    "revision": "1f5c6335186be052c3ba",
+    "url": "./static/js/2.6beddad3.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.007983b3.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.6beddad3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "15402f00778019906b92",
-    "url": "./static/js/main.61eb009d.chunk.js"
+    "revision": "b63cac65fcd072a22b4c",
+    "url": "./static/js/main.5a0c8424.chunk.js"
   },
   {
     "revision": "00722c5035a6ce4d8014",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b405ed957da993f484627507f4e0d154.js"
+  "./precache-manifest.9169ea1f7dc63c619263ea7ba322d446.js"
 );
 
 self.addEventListener('message', (event) => {
